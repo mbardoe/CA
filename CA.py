98d4e03167f511cfab4bb156d5b9429ad621c9f8
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-10-26 11:14:48 -0400

    Made some comments to make things make sense.

--- CA.py
+++ CA.py
@@ -17,14 +17,27 @@ def convertNumberToRule(number):
 	'''This turns a number from 0 to 256 to a list of 0's and 1's.'''
 	ans=[0,0,0,0,0,0,0,0]
 	for i in range(8):
-		digit=number%2
-		ans[7-i]=digit
-		number=number/2
+		digit=number%2 # even or odd
+		ans[7-i]=digit # save answer to even or odd
+		number=number/2 # deal with smaller number
 	return ans
 
 def convertThreeToDecimal(threelist):
-	return 4*threelist[0]+2*threelist[1]+threelist[2]
+	
+	return 4*threelist[0]+2*threelist[1]+1*threelist[2]
 
 def applyRule(number, binaryList):
-	number = 
+	rule_str = format(rule, '#010b') [2:] #github.com #Choate library
 
+	rule = {
+        (2, 2, 2): int(rule_str[0]) + 1,
+        (2, 2, 1): int(rule_str[1]) + 1,
+        (2, 1, 2): int(rule_str[2]) + 1,
+        (2, 1, 1): int(rule_str[3]) + 1,
+        (1, 2, 2): int(rule_str[4]) + 1,
+        (1, 2, 1): int(rule_str[5]) + 1,
+        (1, 1, 2): int(rule_str[6]) + 1,
+        (1, 1, 1): int(rule_str[7]) + 1
+    }
+
+	return rule
