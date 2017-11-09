22b31ad7c93e0b44a539cc0ed053b4fbb675e957
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-11-08 22:03:49 -0500

    Update
    There is an error that it is out of range.

--- CA.py
+++ CA.py
@@ -1,4 +1,9 @@
+# import pyglet
 
+# class Window(pyglet.window.window):
+# 	def__init__(self):
+# 		super(window,self).__init__()
+# 		self.set_size(480,480)
 
 def convertToBinary(string):
 	ans=[]
@@ -15,7 +20,7 @@ def convertToBinary(string):
 
 def convertNumberToRule(number):
 	'''This turns a number from 0 to 256 to a list of 0's and 1's.'''
-	ans=[0,0,0,0,0,0,0,0]
+	ans=[0,0,0,0,0,0,0,0] 
 	for i in range(8):
 		digit=number%2 # even or odd
 		ans[7-i]=digit # save answer to even or odd
@@ -26,18 +31,35 @@ def convertThreeToDecimal(threelist):
 	
 	return 4*threelist[0]+2*threelist[1]+1*threelist[2]
 
-def applyRule(number, binaryList):
-	rule_str = format(rule, '#010b') [2:] #github.com #Choate library
+def applyRule(number, binaryList): #binarylist is what I will be seeing 
+	# This function should do the following:
+	#applyRule(120, [1,1,1,0,1,0,0,1,0,0,0,0,1])
+	#[0,1,0,1,1,0,1,0,0,1,0,0,0]
+	#applyRule(0, [1,1,0,0])
+	#[0,0,0,0]
+	
+	# if number in convertNumberToRule(number) == 0:
+	# 		new_binary_list = 0
+	# elif number in convertNumberToRule(number) == 255:
+	# 		new_binary_list = 1
+	# else: 
+	binaryList[0] == 0
+	binaryList[1] == 0
+	new_binary_list = []
+	for i in range(len(binaryList)-3):
+		if binaryList[i] == 0:
+			if binaryList[i+1] == 0:
+				new_binary_list.append(0)
+			elif binaryList[i+1] == 1:
+				new_binary_list.append(1)
+		elif binaryList[i] == 1:
+			if binaryList[i+1] == 1 and binaryList[i+2] == 1:
+				new_binary_list.append(0)
+			else:
+				new_binary_list.append(1)
 
-	rule = {
-        (2, 2, 2): int(rule_str[0]) + 1,
-        (2, 2, 1): int(rule_str[1]) + 1,
-        (2, 1, 2): int(rule_str[2]) + 1,
-        (2, 1, 1): int(rule_str[3]) + 1,
-        (1, 2, 2): int(rule_str[4]) + 1,
-        (1, 2, 1): int(rule_str[5]) + 1,
-        (1, 1, 2): int(rule_str[6]) + 1,
-        (1, 1, 1): int(rule_str[7]) + 1
-    }
 
-	return rule
+
+
+
+
