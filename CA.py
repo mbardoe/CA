3d4e5eab995813bf5336d9956ed28fe82abc27da
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-11-09 22:48:27 -0500

    This works now.

--- CA.py
+++ CA.py
@@ -1,10 +1,3 @@
-# import pyglet
-
-# class Window(pyglet.window.window):
-# 	def__init__(self):
-# 		super(window,self).__init__()
-# 		self.set_size(480,480)
-
 def convertToBinary(string):
 	ans=[]
 	for letter in string:
@@ -24,11 +17,10 @@ def convertNumberToRule(number):
 	for i in range(8):
 		digit=number%2 # even or odd
 		ans[7-i]=digit # save answer to even or odd
-		number=number/2 # deal with smaller number
+		number=int(number/2) # deal with smaller number
 	return ans
 
 def convertThreeToDecimal(threelist):
-	
 	return 4*threelist[0]+2*threelist[1]+1*threelist[2]
 
 def applyRule(number, binaryList): #binarylist is what I will be seeing 
@@ -37,29 +29,36 @@ def applyRule(number, binaryList): #binarylist is what I will be seeing
 	#[0,1,0,1,1,0,1,0,0,1,0,0,0]
 	#applyRule(0, [1,1,0,0])
 	#[0,0,0,0]
-	
-	# if number in convertNumberToRule(number) == 0:
-	# 		new_binary_list = 0
-	# elif number in convertNumberToRule(number) == 255:
-	# 		new_binary_list = 1
-	# else: 
-	binaryList[0] == 0
-	binaryList[1] == 0
 	new_binary_list = []
-	for i in range(len(binaryList)-3):
-		if binaryList[i] == 0:
-			if binaryList[i+1] == 0:
-				new_binary_list.append(0)
-			elif binaryList[i+1] == 1:
-				new_binary_list.append(1)
-		elif binaryList[i] == 1:
-			if binaryList[i+1] == 1 and binaryList[i+2] == 1:
-				new_binary_list.append(0)
-			else:
-				new_binary_list.append(1)
+	rule = convertNumberToRule(number)
+	binaryList = [0,0] + binaryList
 
+	for i in range(0,len(binaryList)-2):
+		triples = binaryList[i:i+3]
+		#print(triples)
+		answer = convertThreeToDecimal(triples)
+		new_binary_list.append(rule[7-answer])
+		
+	return new_binary_list
+		#Rule 120 
+		# if binaryList[i] == 0:
+		# 	if binaryList[i+1] == 0:
+		# 		new_binary_list.append(0)
+		# 	elif binaryList[i+1] == 1:
+		# 		new_binary_list.append(1)
+		# elif binaryList[i] == 1:
+		# 	if binaryList[i+1] == 1 and binaryList[i+2] == 1:
+		# 		new_binary_list.append(0)
+		# 	else:
+		# 		new_binary_list.append(1)
 
+def main():
+	print(applyRule(120, [1,1,1,0,1,0,0,1,0,0,0,0,1]))
+	print(applyRule(0, [1,1,1,0,1,0,0,1,0,0,0,0,1]))
+	print(applyRule(255, [1,1,1,0,1,0,0,1,0,0,0,0,1]))
+	
 
+main()
 
 
 
