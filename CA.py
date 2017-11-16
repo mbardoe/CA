f541d90700b42b78a838741f4a70bcaeb7cef3e3
Matthew Bardoe <mbardoe@gmail.com>
2017-11-16 11:34:45 -0500

    Moving toward an object oriented approach.  Working on the display. Nothing is tested. Much needs to be.

--- CA.py
+++ CA.py
@@ -1,3 +1,5 @@
+import matplotlib as plt
+import numpy as np
 # import pyglet
 
 # class Window(pyglet.window.window):
@@ -5,68 +7,96 @@
 # 		super(window,self).__init__()
 # 		self.set_size(480,480)
 
-def convertToBinary(string):
-	ans=[]
-	for letter in string:
-		if letter=='A':
-			ans+=[0,0]
-		elif letter=='T':
-			ans+=[1,1]
-		elif letter=='G':
-			ans+=[0,1]
-		else:
-			ans+=[1,0]
-	return ans
+class CA(object):
+    def __init__(self, number, DNA, memory=25):
+        self.number=number
+        self.rule=self.convertNumberToRule(self.number)
+        self.memory=memory
+        self.state=np.array([0,0]+self.convertToBinary(DNA))
+        self.fig=plt.figure()
 
-def convertNumberToRule(number):
-	'''This turns a number from 0 to 256 to a list of 0's and 1's.'''
-	ans=[0,0,0,0,0,0,0,0] 
-	for i in range(8):
-		digit=number%2 # even or odd
-		ans[7-i]=digit # save answer to even or odd
-		number=int(number/2) # deal with smaller number
-	return ans
+    def state(self, state):
+        self.state=state
 
-def convertThreeToDecimal(threelist):
-	
-	return 4*threelist[0]+2*threelist[1]+1*threelist[2]
+    def setup_graphics(self):
 
-def applyRule(number, binaryList):
-    #binarylist is what I will be seeing
-	# This function should do the following:
-	#applyRule(120, [1,1,1,0,1,0,0,1,0,0,0,0,1])
-	#[0,1,0,1,1,0,1,0,0,1,0,0,0]
-	#applyRule(0, [1,1,0,0])
-	#[0,0,0,0]
-	
-	# if number in convertNumberToRule(number) == 0:
-	# 		new_binary_list = 0
-	# elif number in convertNumberToRule(number) == 255:
-	# 		new_binary_list = 1
-	# else:
-    ans=[]
-    rule=convertNumberToRule(number)
-    newBinaryList=[0,0]+binaryList
-    #print(newBinaryList)
-    for x in range(len(binaryList)):
-        segment=newBinaryList[x:x+3]
-        rulenumber=convertThreeToDecimal(segment)
-        #print(x,segment,rule[rulenumber])
-        ans.append(rule[7-rulenumber])
-    return ans
+        self.axes=self.fig.add_subplot(111)
+
+    def display_plot(self):
+        plt.draw()
+
+    def setup_grid(self):
+        self.grid=np.zeros(self.memory, len(self.state))
+        self.grid[0]=self.state
+        self.gen=0
+
+    def update_grid(self, newstate):
+        if self.gen<self.memory:
+            self.gen+=1
+            self.grid[self.gen]=newstate
+        else:
+            self.gen+=1
+            self.grid[0:self.memory-1]=self.grid[1:]
+            self.grid[self.memory-1]=newstate
+
+
+
+
+
+    def convertToBinary(self, string):
+        ans=[]
+        for letter in string:
+            if letter=='A':
+                ans+=[0,0]
+            elif letter=='T':
+                ans+=[1,1]
+            elif letter=='G':
+                ans+=[0,1]
+            else:
+                ans+=[1,0]
+        return ans
+
+    def convertNumberToRule(self, number):
+        '''This turns a number from 0 to 256 to a list of 0's and 1's.'''
+        ans=[0,0,0,0,0,0,0,0]
+        for i in range(8):
+            digit=number%2 # even or odd
+            ans[7-i]=digit # save answer to even or odd
+            number=int(number/2) # deal with smaller number
+        return ans
+
+    def convertThreeToDecimal(self, threelist):
+
+        return 4*threelist[0]+2*threelist[1]+1*threelist[2]
+
+    def applyRule(self,):
+        #binarylist is what I will be seeing
+        # This function should do the following:
+        #applyRule(120, [1,1,1,0,1,0,0,1,0,0,0,0,1])
+        #[0,1,0,1,1,0,1,0,0,1,0,0,0]
+        #applyRule(0, [1,1,0,0])
+        #[0,0,0,0]
+
+        # if number in convertNumberToRule(number) == 0:
+        # 		new_binary_list = 0
+        # elif number in convertNumberToRule(number) == 255:
+        # 		new_binary_list = 1
+        # else:
+        ans=[]
+
+        #print(newBinaryList)
+        for x in range(len(self.state)-2):
+            segment=self.state[x:x+3]
+            rulenumber=self.convertThreeToDecimal(segment)
+            #print(x,segment,rule[rulenumber])
+            ans.append(self.rule[7-rulenumber])
+        self.state=ans
+        self.update_grid(self.state)
+        return self.state
 
 
 def main():
-    #print(convertNumberToRule(120))
-    #print(len([1,1,1,0,1,0,0,1,0,0,0,0,1]))
-    x=applyRule(120, [1,1,1,0,1,0,0,1,0,0,0,0,1])
-    #print(len(x))
-    print(x)
-    x=applyRule(0, [1,1,1,0,1,0,0,1,0,0,0,0,1])
-    print(x)
-    x=applyRule(255, [1,1,1,0,1,0,0,1,0,0,0,0,1])
-    print(x)
-
+    pass
 
 main()
 
